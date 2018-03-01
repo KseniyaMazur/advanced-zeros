02c332d740959db55fa3948d611edd3a2f92184d
KseniyaMazur <ksuha80018@gmail.com>
2018-03-01 18:47:41 +0300

    advanced-zeroc/feature: implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let iBase = base;
+  let zerosCount = number;
+  let countInBase = 0;
+  let countBaseInNumber, iNumber;
+    
+  for (let i = 2; i <= base; i++)
+  {
+    if (iBase % i === 0)
+    {   
+      countInBase = 0;
+      while (iBase % i === 0) {
+        countInBase++;
+        iBase = iBase / i;
+      }
+      countBaseInNumber = 0;
+      iNumber = number;
+      while (Math.floor(iNumber / i) > 0) {
+        countBaseInNumber += Math.floor(iNumber / i);
+        iNumber = Math.floor(iNumber / i);
+      }
+      zerosCount = Math.min(zerosCount, Math.floor(countBaseInNumber / countInBase));
+    }
+  };
+  return zerosCount;
 }
+
+// getZerosCount(16, 16);
